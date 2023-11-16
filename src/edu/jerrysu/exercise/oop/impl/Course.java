d59f6490e9001487a052ed2cf24337c9d8a5d828
JerrySu5379 <jerrysu5379@gmail.com>
2023-11-16 19:56:15 +0800

    update

--- src/edu/jerrysu/exercise/oop/impl/Course.java
+++ src/edu/jerrysu/exercise/oop/impl/Course.java
@@ -7,5 +7,13 @@ public class Course {
     private final int year;
     private int capacity;
     private Set<Homework> homeworks;
-    protected
+    protected Set<Enrollee> enrollees;
+    protected Course(String name, int year) {
+        this(name, year, 0);
+    }
+    protected Course(String name, int year, int capacity) {
+        this.name = name;
+        this.year = year;
+        this.capacity = capacity;
+    }
 }

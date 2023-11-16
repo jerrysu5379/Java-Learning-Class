afa27b97021575386a43207a9ece729d4b25c4b4
JerrySu5379 <jerrysu5379@gmail.com>
2023-11-16 19:21:46 +0800

    update

--- src/edu/jerrysu/exercise/oop/IStudent.java
+++ src/edu/jerrysu/exercise/oop/IStudent.java
@@ -3,5 +3,6 @@ package edu.jerrysu.exercise.oop;
 public interface IStudent {
     void registerForClass(String stuName, String className, int year);
     void dropClass(String stuName, String className, int year);
-
+    void submitHomework(String stuName, String homeworkName,
+                        String answer, String className, int year);
 }
